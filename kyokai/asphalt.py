9fd3440603548123f2a9de5c72b8de591fd32870
SunDwarf <sun@veriny.tf>
2016-05-15 19:20:46 +0100

    Fix missing asyncio.get_event_loop()

--- kyokai/asphalt.py
+++ kyokai/asphalt.py
@@ -28,5 +28,5 @@ class KyoukaiComponent(Component):
         Starts a Kyokai server.
         """
         protocol_factory = partial(KyokaiProtocol, self.app, ctx)
-        server = await get_event_loop().create_server(protocol_factory)
+        server = await asyncio.get_event_loop().create_server(protocol_factory)
         logger.info("Kyokai serving on {}:{}.".format(self.ip, self.port))
