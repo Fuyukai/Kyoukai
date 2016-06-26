1cce8766625e1405ec1fc7aa064c2b1bb90b9d8d
SunDwarf <sun@veriny.tf>
2016-06-26 18:01:39 +0100

    Update status codes

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 ## Kyōkai (境界)
 
-**Issue tracking lives at https://gitlab.com/Fuyukai/Kyoukai. The GitHub is just a mirror.**
-
-Kyōkai is a fast asynchronous Python server-side web framework. It is built upon 
+Kyōkai is a fast asynchronous Python server-side web framework. It is built upon
 [asyncio](https://docs.python.org/3/library/asyncio.html) and `libuv` for an extremely fast web server.
 
 Setting up a Kyōkai app is incredibly simple. Here's a simple server that echoes your client's headers:
--- kyokai/util.py
+++ kyokai/util.py
@@ -9,9 +9,23 @@ VERSIONT = tuple(map(int, VERSION.split('.')))
 
 HTTP_CODES = {
     200: "OK",
+    201: "Created",
+    202: "Accepted",
+    203: "Non-Authoritative Information",
+    204: "No Content",
+    205: "Reset Content",
+    301: "Moved Permenantly",
+    302: "Found",
+    303: "See Other",
+    304: "Not Modified",
     400: "Bad Request",
+    401: "Unauthorized",
+    403: "Forbidden",
     404: "Not Found",
     405: "Method Not Allowed",
+    410: "Gone",
+    413: "Payload Too Large",
+    429: "Too Many Requests",
     500: "Internal Server Error"
 }
 
