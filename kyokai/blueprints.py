3c65798845c119505d6427395f0915ee69bc6a7d
SunDwarf <sun@veriny.tf>
2016-07-21 17:17:41 +0100

    Change this type annotation.

--- kyokai/blueprints.py
+++ kyokai/blueprints.py
@@ -118,7 +118,7 @@ class Blueprint(object):
         self.routes.append(r)
         return r
 
-    def get_errorhandler(self, code: int) -> list:
+    def get_errorhandler(self, code: int) -> Route:
         """
         Not to be used by the user/dev - internal function used to traverse down the parent's error handlers.
         """
