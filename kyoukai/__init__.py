90d2c63bd50438419ba4f752d4ab2e5afc52f147
SunDwarf <sun@veriny.tf>
2016-07-22 15:45:52 +0100

    Import more stuff in kyoukai.__init__.

--- kyoukai/__init__.py
+++ kyoukai/__init__.py
@@ -1,4 +1,11 @@
 from .app import Kyoukai
 from .request import Request
-from .response import Response
+from .response import Response, redirect
 from .context import HTTPRequestContext
+from .blueprints import Blueprint
+from .route import Route
+from .views import View
+from .asphalt import KyoukaiComponent
+from .exc import HTTPException, HTTPClientException
+from .util import VERSION, VERSIONT, static_filename
+from .protocol import KyokaiProtocol
