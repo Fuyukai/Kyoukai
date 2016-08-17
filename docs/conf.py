196315044f65a69a9d947f96afa5ae3dc38ea373
SunDwarf <sun@veriny.tf>
2016-08-17 01:01:30 +0100

    Add type hint ext to docs

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,8 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.asyncio'
+    'sphinxcontrib.asyncio',
+    'sphinx_autodoc_typehints'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
