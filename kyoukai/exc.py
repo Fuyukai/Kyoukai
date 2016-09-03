393f762027e96ba05646506bdd4b6094ee7c4e47
SunDwarf <sun@veriny.tf>
2016-09-03 13:38:06 +0100

    Add abort() function which aborts the current request with a HTTPException.

--- kyoukai/exc.py
+++ kyoukai/exc.py
@@ -14,6 +14,7 @@ class HTTPException(Exception):
         if not some_condition:
             raise HTTPException(404)
     """
+
     def __init__(self, errcode, msg=None,
                  route=None):
         self.code = errcode
@@ -28,9 +29,23 @@ class HTTPException(Exception):
         return "HTTP {} {}".format(self.code, self.msg)
 
 
-def exc_from(exc: Exception):
+def abort(code: int):
+    """
+    Aborts the current request, raising a HTTPException.
+
+    This is similar to Flask's abort method.
+
+    :param code: The code of the HTTPException to create.
+    :raises: A :class:`HTTPException`.
+    """
+    raise HTTPException(code)
+
+
+def exc_from(exc: Exception) -> HTTPException:
     """
     Creates a new HTTP 500 INTERNAL SERVER ERROR exception from a previous exception.
+
+    You probably don't want to use this.
 
     :param exc: The exception to convert.
     :return: The new :class:`HTTPException`.
