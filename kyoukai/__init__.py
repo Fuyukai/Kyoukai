95da5277863497ac9b8ac661f68400dbacd70a67
SunDwarf <sun@veriny.tf>
2017-01-19 12:11:38 +0000

    Add JSON parser to formdata parsers. Closes #20.

--- kyoukai/__init__.py
+++ kyoukai/__init__.py
@@ -1,6 +1,23 @@
 """
 Kyoukai is an async web framework for Python 3.5 and above.
 """
+from werkzeug.exceptions import RequestEntityTooLarge
+from werkzeug.formparser import FormDataParser
+import json
+
+
+def _parse_json(parser: FormDataParser, stream, mimetype, content_length, options):
+    if parser.max_content_length is not None and \
+                    content_length is not None and \
+                    content_length > parser.max_content_length:
+        raise RequestEntityTooLarge()
+
+    # json loads the stream and return it
+    return stream, json.load(stream), {}
+
+
+FormDataParser.parse_functions["application/json"] = _parse_json
+
 from kyoukai.app import Kyoukai, __version__
 from kyoukai.blueprint import Blueprint
 from kyoukai.route import Route
