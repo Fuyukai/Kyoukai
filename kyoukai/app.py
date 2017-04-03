5c3bde48dedfdb2a81d25c8bbc217c6549bf9921
Laura <l@veriny.tf>
2017-04-03 14:21:59 +0100

    Bump version number
    Signed-off-by: Laura <l@veriny.tf>

--- kyoukai/app.py
+++ kyoukai/app.py
@@ -14,7 +14,7 @@ from werkzeug.wrappers import Request, Response
 from kyoukai.asphalt import HTTPRequestContext
 from kyoukai.blueprint import Blueprint
 
-__version__ = "2.1.2"
+__version__ = "2.1.3"
 
 
 class Kyoukai(object):
